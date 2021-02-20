f2624265bb3fbcb2b8be58011b894fc9eedfa2d5
Pedro Ramos <phramoscosta@gmail.com>
2021-02-19 22:07:01 -0300

    NEW: Add logging middleware

--- internal/app.go
+++ internal/app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"conductor/internal/control"
+	"conductor/internal/middleware"
+
 	"log"
 	"net/http"
 
@@ -10,25 +12,26 @@ import (
 
 var router *mux.Router = mux.NewRouter()
 
-func setUpRoutes(apiRouter *mux.Router) {
-
-	control.AddHealthRoutes(apiRouter)
-}
-
 func getAPIRouter() *mux.Router {
-
 	apiRouter := router.PathPrefix("/api").Subrouter()
 	return apiRouter
 }
 
-func main() {
+func setUpRoutes(apiRouter *mux.Router) {
+	control.AddHealthRoutes(apiRouter)
+}
 
+func setUpMiddlewares(apiRouter *mux.Router) {
+	apiRouter.Use(middleware.LoggingMiddleware)
+}
+
+func main() {
 	apiRouter := getAPIRouter()
 	//TODO: Add auth middleware to apiRouter
 
 	setUpRoutes(apiRouter)
+	setUpMiddlewares(apiRouter)
 
 	log.Println("Listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
-
 }
