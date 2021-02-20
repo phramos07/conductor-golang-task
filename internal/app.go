6af79bf73d868db982925c8d69abcc432c873c49
Pedro Ramos <phramoscosta@gmail.com>
2021-02-20 19:29:22 -0300

    ENH: Improves readability with minor changes

--- internal/app.go
+++ internal/app.go
@@ -38,27 +38,38 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const (
+	uiPath        = "./internal/ui/"
+	swaggerPrefix = "/swagger/"
+)
+
+// Base router. Will be used for swaggerUI server
 var router *mux.Router = mux.NewRouter()
 
+// API Router. Will be used for the API endpoints and all the middlewares
+// of logging, in-memory db and auth.
 func getAPIRouter() *mux.Router {
 	apiRouter := router.PathPrefix("/api").Subrouter()
 	return apiRouter
 }
 
+// Sets up routes in the API router
 func setUpRoutes(apiRouter *mux.Router) {
 	control.AddHealthRoutes(apiRouter)
 }
 
+// Sets up middlewares in the API router
 func setUpMiddlewares(apiRouter *mux.Router) {
 	apiRouter.Use(middleware.LoggingMiddleware)
 	apiRouter.Use(middleware.AuthorizationMiddleware)
 }
 
+// APP's entrypoint
 func main() {
 	apiRouter := getAPIRouter()
 
-	fs := http.FileServer(http.Dir("./internal/ui/"))
-	router.PathPrefix("/swagger/").Handler(http.StripPrefix("/swagger/", fs))
+	fs := http.FileServer(http.Dir(uiPath))
+	router.PathPrefix(swaggerPrefix).Handler(http.StripPrefix(swaggerPrefix, fs))
 
 	setUpRoutes(apiRouter)
 	setUpMiddlewares(apiRouter)
